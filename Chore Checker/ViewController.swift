ff1f49846b75319dcf4d08e53bdacbd5d691e25b
Ethan Rudish <eprudish@gmail.com>
2024-09-22 13:34:25 -0700

    Initial Commit

---
+++ Chore Checker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chore Checker
+//
+//  Created by Ethan Rudish on 9/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
